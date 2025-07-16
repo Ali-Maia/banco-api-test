9b3b6068bb5c4016ab4b587b25430978c147b324
Ali-Maia <aliciaengcomp@gmail.com>
2025-07-16 17:40:21 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,14 +1,13 @@
 const request = require('supertest')
+const postLogin = require('../fixtures/postLogin.json')
 
 const obterToken = async (usuario, senha) => {
+  const bodyLogin = { ...postLogin }
   // Capturar o token
   const respostaLogin = await request(process.env.BASE_URL) 
     .post('/login') // método a ser utilizado
     .set('Content-Type', 'application/json') //cabeçalho da requisição e de qual tipo
-    .send({ //O que será mandado para a api
-      'username': usuario,
-      'senha': senha
-    })
+    .send(bodyLogin) // o que será mandado para a API
 
   return respostaLogin.body.token
 }
--- test/login.test.js
+++ test/login.test.js
@@ -1,19 +1,19 @@
 const request = require('supertest')
 const {expect} = require('chai')
 require('dotenv').config()
+const postLogin = require('../fixtures/postLogin.json')
 
 // 1 - mocha para estrutrar e executar os testes
 describe('Login', () => {
   describe('POST /login', () => {
     it('Deve retornar 200 com token em string quando usar credenciais válidas', async () => {
+      const bodyLogin = { ...postLogin }
+      
       // 2 - SUPERTEST para fazer a requisção na API
         const resposta = await request(process.env.BASE_URL) 
           .post('/login') // método a ser utilizado
           .set('Content-Type', 'application/json') //cabeçalho da requisição e de qual tipo
-          .send({ //O que será mandado para a api
-              'username': 'junio.lima',
-              'senha': '123456'
-            })
+          .send(bodyLogin)
       // 3 - CHAI para fazer as validações//asserções
         expect(resposta.status).to.equal(200) // verifica o status code da resposta | validar se é igual
         expect(resposta.body.token).to.be.a('string') // verifica se dentro do corpo da resposta na proprieda token |validar o tipo
